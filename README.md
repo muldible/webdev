2513106f90a8e2f3a961b12bbb041c7c621e1510
Jamie <jamienola@gmail.com>
2016-12-02 15:23:42 -0500

    Creates very simple html structure and gulp integration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# webdev
+# Eric's Web Dev Test Project
+
+This project is for testing basic web development test environments and integration with github.
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// Include gulp
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['']);
+    gulp.watch("dist/**/*.html").on('change', browserSync.reload);
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch(['dist/css/**/*.css'], function () {
+        reload("dist/css/**/*.css")
+    });
+});
+
+// Default Task
+gulp.task('default', ['sass', 'watch', 'browser-sync']);
