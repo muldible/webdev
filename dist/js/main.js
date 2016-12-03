841d74a3a71bf6e6f5819b94e1ec9301900a5ebc
Jamie <jamienola@gmail.com>
2016-12-02 19:50:56 -0500

    Adds animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
 (function () {
 
     console.log("Hey asshole, mind your own business. Errors go here.");
-    var app = angular.module("myApp", []);
+    var app = angular.module("myApp", ['ngAnimate']);
 
 })();
